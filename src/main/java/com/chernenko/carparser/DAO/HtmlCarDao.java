39cd896735ebf700aab7ff4594fe17556f453aa2
DmytroChernenko <dmytro.chernenko@ex.ua>
2016-03-12 15:23:52 +0200

    Create simple classes. There are no implementations.

---
+++ src/main/java/com/chernenko/carparser/DAO/HtmlCarDao.java
@@ -0,0 +1,7 @@
+package com.chernenko.carparser.DAO;
+
+/**
+ * Created by Dmytro on 12.03.16.
+ */
+public class HtmlCarDao {
+}
---
+++ src/main/java/com/chernenko/carparser/Main.java
@@ -0,0 +1,7 @@
+package com.chernenko.carparser;
+
+/**
+ * Created by Dmytro on 12.03.16.
+ */
+public class Main {
+}
